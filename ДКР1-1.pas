319fc63504f3a727b3eed2fa53c92285a4cb4f6c
я саша <144239395+aleksashaaleksasha@users.noreply.github.com>
2023-10-24 18:40:15 +0300

    Add files via upload

---
+++ ДКР1-1.pas
@@ -0,0 +1,10 @@
+﻿var x,y : real;
+begin
+  readln(x);
+  if (x < -10) then y := ln(x)-ln(x)
+    else if ( x < (-1)) then y := Power(x, 0.1*x)/(x*x) - x
+    else if (x < 3) then y := (cos(x)/exp(x))*((-x)/44)
+    else y :=(cos(2*x)/tan(x))*(Power(x, 0.1*x)/log10(x));
+    if y <> y then writeln('При x = ', x:5:1,' функция y не имеет решений')
+    else writeln('При x = ', x:5:1, ' значение функции y = ',y:6:2);
+end.
---
+++ ДКР1-2.pas
@@ -0,0 +1,13 @@
+﻿var x,y : real;
+begin
+  x := -12;
+  while x <= 5 do begin
+    if (x < -10) then y := ln(x)-ln(x)
+    else if ( x < -1) then y := Power(x, 0.1*x)/(x*x) - x
+    else if (x < 3) then y := (cos(x)/exp(x))*((-x)/44)
+    else y :=(cos(2*x)/tan(x))*(Power(x, 0.1*x)/log10(x));
+    if y <> y then writeln('При x = ', x:5:1,' функция y не имеет решений')
+    else writeln('При x = ', x:5:1, ' значение функции y = ',y:6:2);
+    x := x + 0.3;
+    end;
+end.
